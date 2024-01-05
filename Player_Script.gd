2f3489a7aa79accf972ceeb6bdf69d89d9106fe5
SamPollock <samcpollock@gmail.com>
2024-01-05 16:17:11 -0500

    Added grenades and turning

--- Player_Script.gd
+++ Player_Script.gd
@@ -5,9 +5,11 @@ signal grenadeSignal
 
 var speed = 500
 var laser_scene = preload("res://laser.tscn")
+var grenade_scene = preload("res://grenade.tscn")
 
 var canShoot: bool = true;
 var canGrenade: bool = true;
+var grenadeSpeed = 500
 
 var laserSpawners
 
@@ -24,21 +26,38 @@ func _process(_delta):
 	velocity = direction * speed
 	move_and_slide()
 	
-	if (Input.is_action_just_pressed("Action1") and canShoot):
+	if (Input.is_action_just_pressed("Action1") and canShoot): 	# Launching a lazer
 		print("Shooting!")
+		
 		var laserInstance = laser_scene.instantiate()
+		var player_direction = (get_global_mouse_position() - position).normalized() # Set direction for grenade spawn		
+		laserInstance.direction = player_direction
+		laserInstance.look_at(player_direction)
 		laserInstance.position = laserSpawners[randi() % laserSpawners.size()].global_position
 		%Projectiles.add_child(laserInstance)
+		
 		canShoot = false
 		$Timer_Shoot.start()
 		laserSignal.emit()
 		
-	if (Input.is_action_just_pressed("Action2") and canGrenade):
+	if (Input.is_action_just_pressed("Action2") and canGrenade): # Launching a grenade
 		print("Secondary Actioning!")
 		ToggleIsMoving()
 		canGrenade = false
+		
+		var grenadeInstance = grenade_scene.instantiate() as RigidBody2D
+		grenadeInstance.position = laserSpawners[0].global_position
+		var player_direction = (get_global_mouse_position() - position).normalized() # Set direction for grenade spawn
+		
+		grenadeInstance.linear_velocity = player_direction * grenadeInstance.speed
+		
+		%Projectiles.add_child(grenadeInstance)
+		
 		$Timer_Grenade.start(2)
 		grenadeSignal.emit()
+		
+	#rotate
+	look_at(get_global_mouse_position())
 		
 		
 func ToggleIsMoving():
